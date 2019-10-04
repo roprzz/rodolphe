2d23fab16f7c8c65152f30e31d96ba9abb983f4a
Rodolphe PERUZZA <rodolphe.peruzza@gmail.com>
2019-10-04 16:49:22 +0200

    first commit or anything like that

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+import Header from "../components/header";
+
+const IndexPage = () => (
+  <div className="lines">
+  <div className="mainbloc">
+  <Layout>
+    <SEO title="Home" />
+    <div className="container">
+    <h1>I'm Rodolphe.</h1>
+    <h1>Welcome to <span style={{color:"#A2FFD7"}}>rodolphe.dev</span></h1>
+    I take pics on <a href="https://www.instagram.com/roprzz"><span style={{textDecoration:"underline"}}>Instagram</span></a><br/>
+    My previous experience is on <a href="https://www.linkedin.com/in/rperuzza/"><span style={{textDecoration:"underline"}}>LinkedIn</span></a>
+    </div>
+  </Layout>
+  </div>
+  </div>
+)
+
+export default IndexPage
